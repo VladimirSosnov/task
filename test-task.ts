3af93f8655c40a38ee046e12fa215db2a82ad4c8
Vladimir Sosnov <sosnov_90@list.ru>
2024-03-16 16:50:09 +0300

    end

--- test-task.ts
+++ test-task.ts
@@ -1,52 +1,64 @@
 // 1) функция createMap() принимает _любой_ массив объектов и название свойства в этом объекте
 // возвращает объект Map, в котором ключи -- это значения свойства из объекта, а значения -- это сами объекты
-//
+
 // 2) функция createMap() может принять третий аргумент -- ещё одно названия свойства из объекта.
 // В этом случае значениями в объекте Map должны быть значения этого свойства из объекта
-//
+
 // Надо реализовать функцию и типизировать её наилучшим образом
-//
+
 // Примеры использования и тесты ниже
 
-function createMap() {}
+function createMap<T extends object, K extends keyof T, P extends keyof T>(
+  arr: T[],
+  keyProp: K,
+  prop?: P
+): Map<T[K], T | T[P]> {
+  return new Map<T[K], T | T[P]>(
+    arr.map((obj): [T[K], T | T[P]] => {
+      let key = obj[keyProp];
+      let value = prop ? obj[prop] : obj;
+      return [key, value];
+    })
+  );
+}
 
 type Person = {
   id: number;
   name: string;
   age: number;
   passport: string;
-}
+};
 
 const collection: Person[] = [
   {
     id: 12,
-    name: 'Viago',
+    name: "Viago",
     age: 379,
-    passport: '12vi',
+    passport: "12vi",
   },
   {
     id: 34,
-    name: 'Vladislav',
+    name: "Vladislav",
     age: 862,
-    passport: '34vl',
+    passport: "34vl",
   },
   {
     id: 56,
-    name: 'Deacon',
+    name: "Deacon",
     age: 183,
-    passport: '56de',
+    passport: "56de",
   },
   {
     id: 78,
-    name: 'Petyr',
+    name: "Petyr",
     age: 8000,
-    passport: '78pe',
+    passport: "78pe",
   },
 ];
 
 // Пример: создание объекта Map по свойству id
 
-const mapById = createMap(collection, 'id');
+const mapById = createMap(collection, "id");
 
 // Map [
 //   [12, { id: 12, ... }],
@@ -55,12 +67,14 @@ const mapById = createMap(collection, 'id');
 //   [78, { id: 78, ... }],
 // ]
 
-const viago = mapById.get(12) // { id: 12, ... }
-const isNick = mapById.has(0) // false
+const viago = mapById.get(12); // { id: 12, ... }
+const isNick = mapById.has(0); // false
+
+console.log(viago, isNick);
 
 // Пример: создание объекта Map по свойству passport
 
-const mapByPassport = createMap(collection, 'passport');
+const mapByPassport = createMap(collection, "passport");
 
 // Map [
 //   ['12vi', { passport: '12vi', ... }],
@@ -69,12 +83,14 @@ const mapByPassport = createMap(collection, 'passport');
 //   ['78pe', { passport: '78pe', ... }],
 // ]
 
-const deacon = mapByPassport.get('56de') // { passport: '56de', ... }
-const isJackie = mapByPassport.has('00jc') // false
+const deacon = mapByPassport.get("56de"); // { passport: '56de', ... }
+const isJackie = mapByPassport.has("00jc"); // false
+
+console.log(deacon, isJackie);
 
 // Пример: создание объекта Map по свойствам id и name
 
-const namesMapById = createMap(collection, 'id', 'name');
+const namesMapById = createMap(collection, "id", "name");
 
 // Map [
 //   [12, 'Viago'],
@@ -83,17 +99,17 @@ const namesMapById = createMap(collection, 'id', 'name');
 //   [78, 'Petyr'],
 // ]
 
-const vladislavName = namesMapById.get(34) // 'Vladislav'
+const vladislavName = namesMapById.get(34); // 'Vladislav'
 
 // Примеры вызовов для проверки типизации функции createMap()
 
 // 1
-createMap(collection, 'id') // ok
-createMap(collection, 'passport') // ok
-createMap(collection, 'surname') // error
-createMap(collection, 4) // error
+console.log(createMap(collection, "id")); // ok
+console.log(createMap(collection, "passport")); // ok
+// createMap(collection, "surname"); // error
+// createMap(collection, 4); // error
 
 // 2
-createMap(collection, 'id', 'name') // ok
-createMap(collection, 'passport', 'age') // ok
-createMap(collection, 'passport', 'surname') // error
+console.log(createMap(collection, "id", "name")); // ok
+console.log(createMap(collection, "passport", "age")); // ok
+// createMap(collection, "passport", "surname"); // error
